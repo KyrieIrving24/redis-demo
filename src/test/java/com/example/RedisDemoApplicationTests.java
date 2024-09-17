118fc306cde09dad870bf4d8a62e0b486b1a1573
KyrieIrving24 <1024425445@qq.com>
2024-09-17 21:37:56 +0800

    2024.9.17 / 21

--- src/test/java/com/example/RedisDemoApplicationTests.java
+++ src/test/java/com/example/RedisDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.redis.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,15 +10,21 @@ import org.springframework.data.redis.core.RedisTemplate;
 class RedisDemoApplicationTests {
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Test
     void testString() {
-        redisTemplate.opsForValue().set("name", "虎哥");
+        // key一般都是string类型，所以key的序列化一般用
+        redisTemplate.opsForValue().set("name", "虎哥");  //set方法可以接收任何类型的对象，然后帮我们转换成redis可以处理的字节
         System.out.println(redisTemplate.opsForValue().get("name"));
     }
 
-
+    @Test
+    void testSaveUser() {
+        redisTemplate.opsForValue().set("User:100", new User("虎哥", 21));
+        User o = (User) redisTemplate.opsForValue().get("User:100");
+        System.out.printf("o = " + o);
+    }
 
     @Test
     void contextLoads() {
